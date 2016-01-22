ee048a1ce421009b40914fa87411875e748c6cf2
Nick ODell <nickodell@gmail.com>
2016-01-21 19:50:07 -0700

    Fix test script

--- test.sh
+++ test.sh
@@ -10,16 +10,12 @@ PID2=$!
 echo "sleep2 at $PID2"
 
 ./pact $PID1 $PID2
-PACT_PID=$!
-echo "pact at $PACT_PID"
 
 
 ps -A | grep $PID1
 ps -A | grep $PID2
-ps -A | grep $PACT_PID
 
 #read
 
 #ps -A | grep $PID1
 #ps -A | grep $PID2
-#ps -A | grep $PACT_PID
